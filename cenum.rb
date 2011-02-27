5a49e71c08ed2fdee2d41bf3340fd3ceb034632a
Etienne Vallette d'Osia <dohzya@gmail.com>
2011-02-27 11:49:41 +0100

    fix #{name}!

--- cenum.rb
+++ cenum.rb
@@ -3,7 +3,7 @@ def enum(*names)
   names.map(&:to_s).each_with_index do |name, index|
     klass.const_set(name.capitalize, index)
     klass.send(:define_method, "#{name}?", lambda { @value == klass.const_get(name.to_s.capitalize) })
-    klass.send(:define_method, "#{name}!", lambda { @value == klass.const_get(name.to_s.capitalize) })
+    klass.send(:define_method, "#{name}!", lambda { @value = klass.const_get(name.to_s.capitalize) })
   end
   klass.send(:define_method, :initialize, lambda { |name| @value = klass.const_get(name.to_s.capitalize) })
   klass.send(:define_method, :==, lambda { |other| @value == (other.respond_to?(:__value__) ? other.__value__ : other) })
