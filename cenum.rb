246d109486d18139b17d06271328faa8f45fd7dd
Etienne Vallette d'Osia <dohzya@gmail.com>
2011-02-27 12:21:11 +0100

    add class Enum

--- cenum.rb
+++ cenum.rb
@@ -1,14 +1,40 @@
+class Enum
+
+  def self.constant_name(name)
+    name.to_s.upcase
+  end
+
+  def self.[](name)
+    const_get(constant_name(name))
+  end
+
+  def initialize(name)
+    @value = self.class[name]
+  end
+
+  def ==(other)
+    if Enum === other
+      @value == other.__value__
+    else
+      @value == other
+    end
+  end
+
+  protected
+
+  def __value__
+    @value
+  end
+end
+
 class Object
   def enum(*names)
-    klass = Class.new
-    names.map(&:to_s).each_with_index do |name, index|
-      klass.const_set(name.to_s.upcase, index)
-      klass.send(:define_method, "#{name}?", lambda { @value == klass.const_get(name.to_s.upcase) })
-      klass.send(:define_method, "#{name}!", lambda { @value =  klass.const_get(name.to_s.upcase) })
+    klass = Class.new(Enum)
+    names.each_with_index do |name, index|
+      klass.const_set(klass.constant_name(name), index)
+      klass.send(:define_method, "#{name}?", lambda { @value == klass[name] })
+      klass.send(:define_method, "#{name}!", lambda { @value =  klass[name] })
     end
-    klass.send(:define_method, :initialize, lambda { |name| @value = klass.const_get(name.to_s.upcase) })
-    klass.send(:define_method, :==, lambda { |other| @value == (other.respond_to?(:__value__) ? other.__value__ : other) })
-    klass.send(:define_method, :__value__, lambda { @value })
     return klass
   end
 end
